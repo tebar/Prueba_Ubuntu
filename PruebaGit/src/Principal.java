215b722515380d4ed982fa33cd93a4961394bfac
MARTIN <MARTIN@MARTIN-PC>
2014-04-02 20:31:39 +0200

    agregado nuevo hola mundo

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -2,7 +2,8 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo!");
+		System.out.println("Hola Mundo desde Ubuntu!");
+		System.out.println("Hola Mundo desde Windows!");
 
 	}
 
